310fe4e8c18731095693844120a1e7e689f3d295
dfaveris <scenaristeur@gmail.com>
2018-10-01 14:41:40 +0200

    fuseki

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var useLocalEndpoint = true; // lance un endpoint fuseki local (https://github.com/scenaristeur/fuseki)
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
@@ -19,7 +20,27 @@ app.get('*', function(req, res){
   res.sendFile("index.html", {root: '.'});
 });
 
+if (useLocalEndpoint){
+  var path = require('path');
+
+  var serverHome =  path.join(__dirname, '../fuseki/');
+  console.log(serverHome)
+  var FusekiServer = require('fuseki');
+  var fusekiServer = new FusekiServer({'home': serverHome , 'url': 'http://127.0.0.1:3030'});
+  //console.log(fusekiServer)
+  fusekiServer.start().then(function(s){
+      console.log("Serveur Fuseki sur port 3030")
+      //console.log(fusekiServer.datasets())
+  });
+
+
+}
+
 io.sockets.on('connection', function (socket) {
 console.log("connexion de "+socket.id)
-
+console.log(fusekiServer.options.url)
+fusekiServer.datasets().then(function(ds){
+  console.log(ds)
+  socket.send()
+});
 });
